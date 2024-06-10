f9e2c1a891b4d43ca379c98ab19314dac4462b2d
Roderick Wilson <1000181@hr.nl>
2024-06-10 14:33:21 +0200

    database structure netjes gezet.

--- rodeDatabase.py
+++ rodeDatabase.py
@@ -1,12 +1,8 @@
 import sqlite3
-
 from Members import Member
 
-
-
 class  DatabaseManager:
-    dbname = "MealMemberManagement.db"
-    
+    dbname = "MealMemberManagement.db" 
 
     def __init__(self):
         
@@ -18,10 +14,9 @@ class  DatabaseManager:
         self.CreateConsultantTable()
         self.con.commit()
         self.con.close()
-    # self.cur.execute("DROP TABLE Member")
-    # self.cur.execute("DROP TABLE Consultant")
-    # self.cur.execute("DROP TABLE SystemAdmin")
 
+################################################################################################
+# TABLES here below 
     def CreateMemberTable(self):
         self.cur.execute("""
         CREATE TABLE IF NOT EXISTS Member(
@@ -54,237 +49,16 @@ class  DatabaseManager:
         self.cur.execute("""
         CREATE TABLE IF NOT EXISTS Consultant (
             id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-            firstname TEXT NOT NULL,
-            lastname TEXT NOT NULL,
-            username TEXT NOT NULL UNIQUE,
-            password TEXT NOT NULL,
+            firstname VARCHAR(255) NOT NULL,
+            lastname VARCHAR(255) NOT NULL,
+            username VARCHAR(255) UNIQUE,
+            password VARCHAR(255) NOT NULL,
             registrationdate DATE NOT NULL
         )
     """)
 
-   
-
-    def resetPassword(self,newpassword, username):
-        con = sqlite3.connect(self.dbname)
-        self.cur= con.cursor()
-        self.cur.execute("""
-            UPDATE SystemAdmin
-            SET password = ?
-            WHERE username = ?;
-        """, (newpassword, username))
-        con.commit()
-        con.close()
-
-    def createSystemAdmin(self,user):
-        con = sqlite3.connect(self.dbname)
-        self.cur = con.cursor()
-        self.cur.execute("""
-            INSERT INTO SystemAdmin (firstname, lastname, username, password, registrationdate)
-            VALUES (?, ?, ?, ?, ?);
-        """, (user.firstname, user.lastname, user.username, user.password, user.registrationdate))
-        con.commit()
-        con.close()
-
-    def deleteSystemAdmin(self,systemadmin):
-        con = sqlite3.connect(self.dbname)
-        self.cur= con.cursor()
-        self.cur.execute("""
-            DELETE FROM SystemAdmin 
-            WHERE username = ?;
-        """, (systemadmin.username,))
-        con.commit()
-        con.close()
-    # user word gevonden
-    # pak zijn username
-    # geef nieuwe values
-    # geef params aan update()
-    # user = SystemAdmin("newfirst", "newlast",gevondenuser.username, gevondnuser.password)
-    # update(user)
-    def updateSystemAdmin(self,user, id):
-        con = sqlite3.connect(self.dbname)
-        self.cur= con.cursor()
-        self.cur.execute("""
-            UPDATE SystemAdmin
-            SET firstname = ?, lastname = ?, username = ?
-            WHERE id = ?;
-        """, (user.firstname, user.lastname, user.username, id))
-        con.commit()
-        con.close()
-    
-    def FetchSystemAdminID(self, user):
-        con = sqlite3.connect(self.dbname)
-        self.cur= con.cursor()
-        self.cur.execute("""
-            SELECT id FROM SystemAdmin
-            WHERE username = ?
-        """, (user.username,))
-        id = self.cur.fetchone()[0]
-        con.commit()
-        con.close()
-        return id
-    
-    def FetchMemberMobile(self, mobileNumber):
-        con = sqlite3.connect(self.dbname)
-        self.cur= con.cursor()
-        self.cur.execute("""
-            SELECT mobile FROM Member
-            WHERE mobile = ?
-        """, (mobileNumber,))
-        mobile = self.cur.fetchone()
-        con.commit()
-        con.close()
-        return mobile
-    
-    def FetchMemberEmail(self, email):
-        con = sqlite3.connect(self.dbname)
-        self.cur= con.cursor()
-        self.cur.execute("""
-            SELECT email FROM Member
-            WHERE email = ?
-        """, (email,))
-        mobile = self.cur.fetchone()
-        con.commit()
-        con.close()
-        return mobile
-
-    def resetSystemAdminPassword(self,sytemadmin):
-        con = sqlite3.connect(self.dbname)
-        self.cur= con.cursor()
-        self.cur.execute("""
-            UPDATE SystemAdmin
-            SET password = ?
-            WHERE username = ?;
-        """, (sytemadmin.password, sytemadmin.username))
-        con.commit()
-        con.close()
-
-    def getallUsers(self):
-        from User import SystemAdmin,Consultant
-        con = sqlite3.connect(self.dbname)
-        self.cur = con.cursor()
-        self.cur.execute("""
-            SELECT * FROM SystemAdmin;
-        """)
-        listAdmins = self.cur.fetchall()
-        self.cur.execute("""
-            SELECT * FROM Consultant;
-        """)
-        listCons = self.cur.fetchall()
-        listUsers = []
-        for res in listAdmins:
-            systemAdmin = SystemAdmin(res[1], res[2], res[3], res[4])
-            listUsers.append(systemAdmin)
-        for res in listCons:
-            consultant = Consultant(res[1], res[2], res[3], res[4])
-            listUsers.append(consultant)
-        
-        # for user in listUsers:
-        #     print(user.firstname, user.lastname, user.username, user.password, user.registrationdate)
-        con.close()
-        return listUsers
-
-    def createConsultant(self,user):
-        con = sqlite3.connect(self.dbname)
-        self.cur = con.cursor()
-        self.cur.execute("""
-            INSERT INTO Consultant (firstname, lastname, username, password, registrationdate)
-            VALUES (?, ?, ?, ?, ?);
-        """, (user.firstname, user.lastname, user.username, user.password, user.registrationdate))
-        con.commit()
-        con.close()
-
-    def deleteConsultant(self,consultant):
-        con = sqlite3.connect(self.dbname)
-        self.cur = con.cursor()
-        self.cur.execute("""
-            DELETE FROM Consultant 
-            WHERE username = ?;
-        """, (consultant.username,))
-        con.commit()
-        con.close()
-
-    def updateConsultant(self, user, id):
-        con = sqlite3.connect(self.dbname)
-        self.cur = con.cursor()
-        self.cur.execute("""
-            UPDATE Consultant
-            SET firstname = ?, lastname = ?, username = ?
-            WHERE id = ?;
-        """, (user.firstname, user.lastname, user.username, id))
-        con.commit()
-        con.close()
-
-    def FetchConsultantID(self, user):
-        con = sqlite3.connect(self.dbname)
-        self.cur= con.cursor()
-        self.cur.execute("""
-            SELECT id FROM Consultant
-            WHERE username = ?
-        """, (user.username,))
-        id = self.cur.fetchone()[0]
-        con.commit()
-        con.close()
-        return id
-
-    def resetConsultantPassword(self, consultant):
-        con = sqlite3.connect(self.dbname)
-        self.cur = con.cursor()
-        self.cur.execute("""
-            UPDATE Consultant
-            SET password = ?
-            WHERE username = ?;
-        """, (consultant.password, consultant.username))
-        con.commit()
-        con.close()
-
-    def createMember(self, member):
-        con = sqlite3.connect(self.dbname)
-        self.cur = con.cursor()
-        self.cur.execute("""
-            INSERT INTO Member (membershipID, firstname, lastname, registrationdate, age, gender, weight, adress, email, mobile)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
-        """, (member.membershipID, member.firstname, member.lastname, member.registrationdate, member.age, member.gender, member.weight, member.adress, member.email, member.mobile))
-        con.commit()
-        con.close()
-
-    def deleteMember(self, member):
-        con = sqlite3.connect(self.dbname)
-        self.cur = con.cursor()
-        self.cur.execute("""
-            DELETE FROM Member 
-            WHERE membershipID = ?;
-        """, (member.membershipID,))
-        con.commit()
-        con.close()
-
-    def updateMember(self, member):
-        con = sqlite3.connect(self.dbname)
-        self.cur = con.cursor()
-        self.cur.execute("""
-            UPDATE Member
-            SET firstname = ?, lastname = ?, age = ?, gender = ?, weight = ?, adress = ?, email = ?, mobile = ?
-            WHERE membershipID = ?;
-        """, (member.firstname, member.lastname, member.age, member.gender, member.weight, member.adress, member.email, member.mobile, member.membershipID))
-        con.commit()
-        con.close()
-
-    def getallMembers(self):
-        con = sqlite3.connect(self.dbname)
-        self.cur = con.cursor()
-        self.cur.execute("""
-            SELECT * FROM Member;
-        """)
-        listMems = self.cur.fetchall()
-        listUsers = []
-        for mem in listMems:
-            member = Member(mem[1], mem[2], mem[3], mem[4], mem[5], mem[6], mem[7], mem[8])
-            member.membershipID = mem[0]
-            listUsers.append(member)
-        # for member in listUsers:
-            # print(member.membershipID, member.firstname, member.lastname, member.registrationdate, member.age, member.gender, member.weight, member.adress, member.email, member.mobile)
-        con.close()
-        return listUsers
-
+################################################################################################
+# LOGIN here below 
     def loginUser(self, username, password):
         from User import Consultant, SuperAdmin, SystemAdmin
 
@@ -318,36 +92,249 @@ class  DatabaseManager:
         else:
             return False, None
 
-# db = DatabaseManager()
-# admin = SystemAdmin("firstadmin", "lastadmin", "testadmin", "test")
-# admin1 = SystemAdmin("firstadmin1", "lastadmin1", "testadmin1", "test")
-# admin2 = SystemAdmin("firstadmin2", "lastadmin2", "testadmin2", "test")
-# # createSystemAdmin(admin)
-# # createSystemAdmin(admin1)
-# # createSystemAdmin(admin2)
+################################################################################################
+# CREATE here below   
+    def createMember(self, member):
+        con = sqlite3.connect(self.dbname)
+        self.cur = con.cursor()
+        self.cur.execute("""
+            INSERT INTO Member (membershipID, firstname, lastname, registrationdate, age, gender, weight, adress, email, mobile)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+        """, (member.membershipID, member.firstname, member.lastname, member.registrationdate, member.age, member.gender, member.weight, member.adress, member.email, member.mobile))
+        con.commit()
+        con.close()
 
-# consulant = Consultant("firstconsul", "lastconsul", "testconsul", "test")
-# consulant1 = Consultant("firstconsul1", "lastconsul1", "testconsul1", "test")
-# consulant2 = Consultant("firstconsul2", "lastconsul2", "testconsul2", "test")
-# # createConsultant(consulant)
-# # createConsultant(consulant1)
-# # createConsultant(consulant2)
+    def createConsultant(self,user):
+        con = sqlite3.connect(self.dbname)
+        self.cur = con.cursor()
+        self.cur.execute("""
+            INSERT INTO Consultant (firstname, lastname, username, password, registrationdate)
+            VALUES (?, ?, ?, ?, ?);
+        """, (user.firstname, user.lastname, user.username, user.password, user.registrationdate))
+        con.commit()
+        con.close()
 
-# member = Member('firstmember', 'lastmember', 30, 'M', 61.4, 'Streetname housenumber zipcode City', 'my@email.com', +31612345678)
-# member1 = Member('firstmember1', 'lastmember1', 30, 'M', 61.4, 'Streetname housenumber zipcode City', 'my@email1.com', +31612345671)
-# member2 = Member('firstmember2', 'lastmember2', 30, 'M', 61.4, 'Streetname housenumber zipcode City', 'my@email2.com', +31612345672)
-# # createMember(member)
-# # createMember(member1)
-# # createMember(member2)
+    def createSystemAdmin(self,user):
+        con = sqlite3.connect(self.dbname)
+        self.cur = con.cursor()
+        self.cur.execute("""
+            INSERT INTO SystemAdmin (firstname, lastname, username, password, registrationdate)
+            VALUES (?, ?, ?, ?, ?);
+        """, (user.firstname, user.lastname, user.username, user.password, user.registrationdate))
+        con.commit()
+        con.close()
 
-# db.getallSystemAdmins()
-# getallConsultants()
-# getallMembers()
+################################################################################################
+# UPDATE here below 
+    def updateMember(self, member):
+        con = sqlite3.connect(self.dbname)
+        self.cur = con.cursor()
+        self.cur.execute("""
+            UPDATE Member
+            SET firstname = ?, lastname = ?, age = ?, gender = ?, weight = ?, adress = ?, email = ?, mobile = ?
+            WHERE membershipID = ?;
+        """, (member.firstname, member.lastname, member.age, member.gender, member.weight, member.adress, member.email, member.mobile, member.membershipID))
+        con.commit()
+        con.close()
 
-# deleteSystemAdmin("testingadmin")
-# deleteConsultant(consulant2.username)
-# deleteMember()
+    def updateConsultant(self, user, id):
+        con = sqlite3.connect(self.dbname)
+        self.cur = con.cursor()
+        self.cur.execute("""
+            UPDATE Consultant
+            SET firstname = ?, lastname = ?, username = ?
+            WHERE id = ?;
+        """, (user.firstname, user.lastname, user.username, id))
+        con.commit()
+        con.close()
 
+    def updateSystemAdmin(self,user, id):
+        con = sqlite3.connect(self.dbname)
+        self.cur= con.cursor()
+        self.cur.execute("""
+            UPDATE SystemAdmin
+            SET firstname = ?, lastname = ?, username = ?
+            WHERE id = ?;
+        """, (user.firstname, user.lastname, user.username, id))
+        con.commit()
+        con.close()
 
-# resetSystemAdminPassword("newpassword", "testadmin")
-# resetConsultantPassword("newpassword", "testconsul")
+################################################################################################
+# RESET here below     
+    def resetPassword(self,newpassword, username):
+        con = sqlite3.connect(self.dbname)
+        self.cur= con.cursor()
+        self.cur.execute("""
+            UPDATE SystemAdmin
+            SET password = ?
+            WHERE username = ?;
+        """, (newpassword, username))
+        con.commit()
+        con.close()
+
+    def resetConsultantPassword(self, consultant):
+        con = sqlite3.connect(self.dbname)
+        self.cur = con.cursor()
+        self.cur.execute("""
+            UPDATE Consultant
+            SET password = ?
+            WHERE username = ?;
+        """, (consultant.password, consultant.username))
+        con.commit()
+        con.close()
+
+    def resetSystemAdminPassword(self,sytemadmin):
+        con = sqlite3.connect(self.dbname)
+        self.cur= con.cursor()
+        self.cur.execute("""
+            UPDATE SystemAdmin
+            SET password = ?
+            WHERE username = ?;
+        """, (sytemadmin.password, sytemadmin.username))
+        con.commit()
+        con.close()
+
+################################################################################################
+# DELETE here below
+    def deleteMember(self, member):
+        con = sqlite3.connect(self.dbname)
+        self.cur = con.cursor()
+        self.cur.execute("""
+            DELETE FROM Member 
+            WHERE membershipID = ?;
+        """, (member.membershipID,))
+        con.commit()
+        con.close()
+
+    def deleteConsultant(self,consultant):
+        con = sqlite3.connect(self.dbname)
+        self.cur = con.cursor()
+        self.cur.execute("""
+            DELETE FROM Consultant 
+            WHERE username = ?;
+        """, (consultant.username,))
+        con.commit()
+        con.close()
+
+    def deleteSystemAdmin(self,systemadmin):
+        con = sqlite3.connect(self.dbname)
+        self.cur= con.cursor()
+        self.cur.execute("""
+            DELETE FROM SystemAdmin 
+            WHERE username = ?;
+        """, (systemadmin.username,))
+        con.commit()
+        con.close()
+
+################################################################################################
+# GET here below
+    def getallUsers(self):
+        from User import SystemAdmin,Consultant
+        con = sqlite3.connect(self.dbname)
+        self.cur = con.cursor()
+        self.cur.execute("""
+            SELECT * FROM SystemAdmin;
+        """)
+        listAdmins = self.cur.fetchall()
+        self.cur.execute("""
+            SELECT * FROM Consultant;
+        """)
+        listCons = self.cur.fetchall()
+        listUsers = []
+        for res in listAdmins:
+            systemAdmin = SystemAdmin(res[1], res[2], res[3], res[4])
+            listUsers.append(systemAdmin)
+        for res in listCons:
+            consultant = Consultant(res[1], res[2], res[3], res[4])
+            listUsers.append(consultant)
+        con.close()
+        return listUsers
+
+    def getallMembers(self):
+        con = sqlite3.connect(self.dbname)
+        self.cur = con.cursor()
+        self.cur.execute("""
+            SELECT * FROM Member;
+        """)
+        listMems = self.cur.fetchall()
+        listUsers = []
+        for mem in listMems:
+            member = Member(mem[1], mem[2], mem[3], mem[4], mem[5], mem[6], mem[7], mem[8])
+            member.membershipID = mem[0]
+            listUsers.append(member)
+        con.close()
+        return listUsers
+
+################################################################################################
+# FETCHES here below
+    def FetchMemberMobile(self, mobileNumber):
+        con = sqlite3.connect(self.dbname)
+        self.cur= con.cursor()
+        self.cur.execute("""
+            SELECT mobile FROM Member
+            WHERE mobile = ?
+        """, (mobileNumber,))
+        mobile = self.cur.fetchone()
+        con.commit()
+        con.close()
+        return mobile
+    
+    def FetchMemberEmail(self, email):
+        con = sqlite3.connect(self.dbname)
+        self.cur= con.cursor()
+        self.cur.execute("""
+            SELECT email FROM Member
+            WHERE LOWER(email) = LOWER(?)
+        """, (email,))
+        mobile = self.cur.fetchone()
+        con.commit()
+        con.close()
+        return mobile
+    
+    def FetchConsUsername(self, consusername):
+        con = sqlite3.connect(self.dbname)
+        self.cur= con.cursor()
+        self.cur.execute("""
+            SELECT username FROM Consultant
+            WHERE LOWER(username) = LOWER(?)
+        """, (consusername,))
+        username = self.cur.fetchone()
+        con.commit()
+        con.close()
+        return username
+    
+    def FetchConsultantID(self, user):
+        con = sqlite3.connect(self.dbname)
+        self.cur= con.cursor()
+        self.cur.execute("""
+            SELECT id FROM Consultant
+            WHERE LOWER(username) = LOWER(?)
+        """, (user.username,))
+        id = self.cur.fetchone()[0]
+        con.commit()
+        con.close()
+        return id
+    
+    def FetchSystemAdminID(self, user):
+        con = sqlite3.connect(self.dbname)
+        self.cur= con.cursor()
+        self.cur.execute("""
+            SELECT id FROM SystemAdmin
+            WHERE LOWER(username) = LOWER(?)
+        """, (user.username,))
+        id = self.cur.fetchone()[0]
+        con.commit()
+        con.close()
+        return id
+    
+    def FetchAdminUsername(self, adminusername):
+        con = sqlite3.connect(self.dbname)
+        self.cur= con.cursor()
+        self.cur.execute("""
+            SELECT username FROM SystemAdmin
+            WHERE LOWER(username) = LOWER(?)
+        """, (adminusername,))
+        username = self.cur.fetchone()
+        con.commit()
+        con.close()
+        return username
